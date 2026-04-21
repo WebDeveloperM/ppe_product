c83296d81d6b856c430723c8f3ac4e058c8c2ade
Ma'ruf Shabonov <shabonov.m@bnpz.local>
2026-04-21 11:30:02 +0500

    last

--- backend/base/migrations/0034_pendingitemissue_qr_fields.py
+++ backend/base/migrations/0034_pendingitemissue_qr_fields.py
@@ -2,6 +2,14 @@ from django.db import migrations, models
 import uuid
 
 
+def populate_pending_issue_qr_tokens(apps, schema_editor):
+    PendingItemIssue = apps.get_model('base', 'PendingItemIssue')
+
+    for pending_issue in PendingItemIssue.objects.filter(qr_token__isnull=True).iterator():
+        pending_issue.qr_token = uuid.uuid4()
+        pending_issue.save(update_fields=['qr_token'])
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -22,11 +30,17 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='pendingitemissue',
             name='qr_token',
-            field=models.UUIDField(db_index=True, default=uuid.uuid4, editable=False, unique=True, verbose_name='QR токен выдачи'),
+            field=models.UUIDField(blank=True, null=True, db_index=True, editable=False, verbose_name='QR токен выдачи'),
         ),
         migrations.AddField(
             model_name='pendingitemissue',
             name='warehouse_signed_at',
             field=models.DateTimeField(blank=True, null=True, verbose_name='Дата подписи кладовщика'),
+        ),
+        migrations.RunPython(populate_pending_issue_qr_tokens, migrations.RunPython.noop),
+        migrations.AlterField(
+            model_name='pendingitemissue',
+            name='qr_token',
+            field=models.UUIDField(db_index=True, default=uuid.uuid4, editable=False, unique=True, verbose_name='QR токен выдачи'),
         ),
     ]
